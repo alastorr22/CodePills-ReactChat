dfcb33295fb864008daf476cd5f432364d49a783
alastorr22 <marcomontesgarnica@gmail.com>
2023-12-01 09:37:59 -0400

    proyectoChatReal

--- front/src/App.js
+++ front/src/App.js
@@ -2,14 +2,23 @@ import './App.css';
 import { io } from 'socket.io-client';
 import { useState, useEffect } from 'react';
 import { LiMensaje, UlMensajes } from './ui-components';
-
+import logo from './perfil.png'; 
 const socket = io('http://localhost:3000');
 
-function App() {
 
+const initialStateValues = {
+  usuario: '',
+  mensaje: ''
+};
+
+function App() {
+  const [nombreUsuario, setNombreUsuario] = useState('');
   const [isConnected, setIsConnected] = useState(false);
   const [nuevoMensaje, setNuevoMensaje] = useState('');
   const [mensajes, setMensajes] = useState([]);
+
+
+  const [values, setValues] = useState(initialStateValues);
 
   useEffect(() => {
 
@@ -26,28 +35,51 @@ function App() {
 
   }, []);
 
+  const handleEnviar = (e) => {
+    e.preventDefault();
+    console.log(values)
+  }
+
+
   const enviarMensaje = () => {
     socket.emit('chat_message', {
-      usuario: socket.id,
+      usuario: nombreUsuario,
       mensaje: nuevoMensaje
     });
   }
-
   return (
     <div className="App">
-      <h2>{isConnected ? 'CONECTADO' : 'NO CONECTADO'}</h2>
-      <UlMensajes>
-        {mensajes.map(mensaje => (
-          <LiMensaje>{mensaje.usuario}: {mensaje.mensaje}</LiMensaje>
-        ))}
-      </UlMensajes>
-      <input
-        type="text"
-        onChange={e => setNuevoMensaje(e.target.value)}
-      />
-      <button onClick={enviarMensaje}>Enviar</button>
+      <div className='mensaje'>
+        <h3>Nombre de Usuario: {nombreUsuario}</h3>
+        <UlMensajes>
+          {mensajes.map(mensaje => (
+            <LiMensaje key={mensaje.id}>{mensaje.usuario}: {mensaje.mensaje}</LiMensaje>
+          ))}
+        </UlMensajes>
+      </div>
+      <form className='login' onSubmit={handleEnviar}>
+        <img className='logo' src={logo} alt="Logo de perfil" />
+        <input
+          className='input'
+          type="text"
+          placeholder="Ingresa tu nombre de usuario"
+          value={nombreUsuario}
+          onChange={e => setNombreUsuario(e.target.value)}
+        />
+        <br />
+        <input
+          className='input'
+          type="text"
+          placeholder="Escribe un mensaje..."
+          value={nuevoMensaje}
+          onChange={e => setNuevoMensaje(e.target.value)}
+        />
+        <br />
+        <button className='boton' onClick={enviarMensaje} >Enviar</button>
+      </form>
     </div>
   );
+
 }
 
 export default App;
